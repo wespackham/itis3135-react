b6f8f721d230573f616305dcb9e2d86ea553d568
Wes Packham <wesleypackham@gmail.com>
2025-11-11 17:24:52 -0500

    Added files

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import Introduction from './pages/Introduction'
+import Contract from './pages/Contract'
+import './App.css'
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="/Home" element={<Home />} />
+                <Route path="/Introduction" element={<Introduction />} />
+                <Route path="/Contract" element={<Contract />} />
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
+}
+
+export default App
